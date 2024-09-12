e9c7a1360a8a7123e2d46dab17e4acfd4cd677c9
Kristian Kirchev <kris.kirchev@gmail.com>
2024-09-12 14:08:40 +0300

    lazy

---
+++ src/main/java/com/tribrine/practice/lazy/Application.java
@@ -0,0 +1,16 @@
+package com.tribrine.practice.lazy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+
+        ConfigurableApplicationContext context = SpringApplication.run(Application.class, args);
+
+        LazyLoader lazyLoader = context.getBean(LazyLoader.class);
+    }
+}
